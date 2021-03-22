9473d5727d1d36d48ca6c8d26948ab22bfa3ac85
FlorimelF <flotte.florimel@gmail.com>
2021-03-22 09:44:06 +0100

    Add trigger 9 et 12 - contrainte

--- Trigger.sql
+++ Trigger.sql
@@ -1,5 +1,21 @@
+/* Trigger 9 */
+drop trigger VerificationDateExp;
 create trigger VerificationDateExp before insert on EXPERIENCE for each row
 begin
     if :new.DEBUTEXP > :new.FINEXP then
         raise_application_error(-2001, 'Les dates pour l experience ne sont pas dans le bon ordre');
+    end if;
 end;
+/
+
+/* Trigger 12 */
+drop trigger VerificationDateDemande;
+create trigger VerificationDateDemande before insert on EXPERIENCE for each row
+begin
+    if :new.DEBUTEXP < :new.DATECHERCHEUR then
+        raise_application_error(-2001, 'Les dates ne sont pas dans le bon sens');
+    end if;    
+end;
+/
+
+commit;
