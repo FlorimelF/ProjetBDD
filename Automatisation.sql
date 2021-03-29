7797ef8e869d9935f2aa12c44abc25849887649f
EnolaRdt <roudautenola@gmail.com>
2021-03-29 11:16:23 +0200

    Modification page trigger

---
+++ Automatisation.sql
@@ -0,0 +1,13 @@
+drop trigger AutoIndicePriorité;
+create trigger AutoIndicePriorité before insert on EXPERIENCE for each row
+begin
+
+    for i in 1..(select count(*) from EXPERIENCE ) loop
+        select
+end;
+/
+
+
+
+
+
--- Trigger.sql
+++ Trigger.sql
@@ -1,6 +1,6 @@
 /* Trigger 9 */
 drop trigger T_VerificationDateExp;
-create trigger VerificationDateExp before insert on EXPERIENCE for each row
+create trigger T_VerificationDateExp before insert on EXPERIENCE for each row
 begin
     if :new.DEBUTEXP > :new.FINEXP then
         raise_application_error(-2001, 'Les dates pour l experience ne sont pas dans le bon ordre');
@@ -10,11 +10,24 @@ end;
 
 /* Trigger 12 */
 drop trigger T_VerificationDateDemande;
-create trigger VerificationDateDemande before insert on EXPERIENCE for each row
+create trigger T_VerificationDateDemande before insert on EXPERIENCE for each row
 begin
     if :new.DEBUTEXP < :new.DATECHERCHEUR then
         raise_application_error(-2001, 'Les dates ne sont pas dans le bon sens');
     end if;    
+end;
+/
+
+commit;
+
+
+/* Trigger 13 */
+drop trigger T_VerificationNiveauAcceptation;
+create trigger T_VerificationNiveauAcceptation before insert on EXPERIENCE for each row
+begin
+    if :new.a1 > :new.a2 then
+        raise_application_error(-2001, 'Attention, a1 doit être < ou = de a2');
+end if;
 end;
 /
 
