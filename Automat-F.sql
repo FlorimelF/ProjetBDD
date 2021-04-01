ee20fcf52ef6758b55c0ef0136b207ff8027df4e
FlorimelF <flotte.florimel@gmail.com>
2021-04-01 11:46:44 +0200

    Ajout de l'automatisation pour le placement des groupes

--- Automat-F.sql
+++ Automat-F.sql
@@ -1,7 +1,37 @@
-/* Trigger */
+/* Trigger Placement des n replicas d'un groupe sur des plaques. 
+Un groupe doit être sur la même plaque. 
+Tous les roupes d'une même plaque sont sous les mêmes conditions 
+(opacimétrique/colorimétrique, suivis dans le temps/non suivis dans 
+le temps) */
 drop trigger PlacementGroupe;
-create trigger PlacementGroupe after insert on EXPERIENCE for each row
+create trigger PlacementGroupe before insert on GROUPE for each row
+declare
+    nb NUMBER;
+    NbSlotOcc NUMBER;
 begin
-    
+    SELECT NBSLOTPOURGROUPE 
+        INTO nb 
+        FROM GROUPE 
+        NATURAL JOIN EXPERIENCE
+        WHERE IDEXPERIENCE = :new.IDEXPERIENCE;
+    for i in (SELECT IDPLAQUE, IDTYPEPLAQUE FROM PLAQUE NATURAL JOIN LOTPLAQUE) loop
+        SELECT SUM(NBSLOTPOURGROUPE) 
+            INTO NbSlotOcc 
+            FROM EXPERIENCE 
+            NATURAL JOIN GROUPE 
+            WHERE IDPLAQUE = i.IDPLAQUE;
+        if i.IDTYPEPLAQUE = 0 then
+            if NbSlotOcc <= 96 - nb then
+                :new.IDPLAQUE := i.IDPLAQUE;
+                EXIT;
+            end if;
+        else
+            if NbSlotOcc <= 384 - nb then
+                :new.IDPLAQUE := i.IDPLAQUE;
+                EXIT;
+            end if;
+        end if;
+    end loop;
 end;
 /
+
