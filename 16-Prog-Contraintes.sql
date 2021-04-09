2ea88ee238baf383ec2d5973f2cf161cdc7a6206
EnolaRdt <roudautenola@gmail.com>
2021-04-09 17:59:31 +0200

    Maj Contraintes + Test Enola

--- 16-Prog-Contraintes.sql
+++ 16-Prog-Contraintes.sql
@@ -7,8 +7,8 @@ drop trigger T_VerificationDateExp;
 create trigger T_VerificationDateExp before insert on EXPERIENCE for each row
 begin
     if :new.DEBUTEXP > :new.FINEXP then
-        raise_application_error(-2001, 'Les dates pour l experience ne sont pas dans le bon ordre');
-    end if;
+        raise_application_error(-20001, 'Les dates pour l experience ne sont pas dans le bon ordre');
+end if;
 end;
 /
 
@@ -17,7 +17,7 @@ drop trigger T_VerificationDateDemande;
 create trigger T_VerificationDateDemande before insert on EXPERIENCE for each row
 begin
     if :new.DEBUTEXP < :new.DATECHERCHEUR then
-        raise_application_error(-2001, 'Les dates ne sont pas dans le bon sens');
+        raise_application_error(-20001, 'Les dates ne sont pas dans le bon sens');
     end if;    
 end;
 /
@@ -30,7 +30,7 @@ drop trigger T_VerificationNiveauAcceptation;
 create trigger T_VerificationNiveauAcceptation before insert on EXPERIENCE for each row
 begin
     if :new.a1 > :new.a2 then
-        raise_application_error(-2001, 'Attention, a1 doit être < ou = de a2');
+        raise_application_error(-20001, 'Attention, a1 doit être < ou = de a2');
 end if;
 end;
 /
@@ -47,7 +47,7 @@ begin
 select count(*) into nbReac from
 (select count (*) from REACTIF group by nomReactif having count(*) > 1) maTable;
 if nbReac = 1 then
-        raise_application_error(-2001, 'Ce nom de réactif existe déjà');
+        raise_application_error(-20001, 'Ce nom de réactif existe déjà');
 end if;
 end;
 /
