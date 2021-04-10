04d9c75a7b74a6b7a85298811d0d66184bde032b
FlorimelF <flotte.florimel@gmail.com>
2021-04-10 16:03:26 +0200

    Commit test en +

--- Test-Flo.sql
+++ Test-Flo.sql
@@ -4,13 +4,13 @@ Si le nom du réactif utilisé existe déjà dans la BDD,
 il n'est pas à nouveau inséré pour éviter les redondances 
 (ne pas tenir compte de la casse, ni des accents) 
 */ 
-drop table resultat_test;
-create table resultat_test (nomTest varchar2(100), resultat number(1) check(resultat in (0,1)));
+drop table TraceTest;
+create table TraceTest (nomTest varchar2(100), resultat number(1) check(resultat in (0,1)));
 
 /**
   Test positif pour 
  */
-create or replace procedure Test_T_IntegrationTReactifP as
+create or replace procedure Test_PlacementGroupe as
     CHECK_CONSTRAINT_VIOLATED EXCEPTION;
     pragma exception_init(check_constraint_violated, -20001) ;
 
@@ -27,17 +27,17 @@ commit;
     'Reactif2'
   );
   rollback;
-  insert into resultat_test values ('Test_T_IntegrationTReactifP',1);
+  insert into TraceTest values ('Test_PlacementGroupe',1);
   commit;
   exception
     when check_constraint_violated then
       rollback;
-      insert into resultat_test values ('Test_T_IntegrationTReactifP',0);
+      insert into TraceTest values ('Test_PlacementGroupe',0);
       commit; 
 end;
 /
 begin
-  Test_T_IntegrationTReactifP;
+  Test_PlacementGroupe;
 end;
 /
 commit;
@@ -45,7 +45,7 @@ commit;
  /**
   Test negatif pour 
  */
- create or replace procedure Test_T_IntegrationTReactifN as
+ create or replace procedure Test_PlacementGroupeN as
     CHECK_CONSTRAINT_VIOLATED EXCEPTION;
     pragma exception_init(check_constraint_violated, -20001) ;
 
@@ -62,18 +62,18 @@ commit;
     'Reactif1'
   );
   rollback;
-  insert into resultat_test values ('Test_T_IntegrationTReactifN',0);
+  insert into TraceTest values ('Test_PlacementGroupeN',0);
   commit;
   exception
     when check_constraint_violated then
       rollback;
-      insert into resultat_test values ('Test_T_IntegrationTReactifN',1);
+      insert into TraceTest values ('Test_PlacementGroupeN',1);
       commit; 
 end;
 /
 begin
-  Test_T_IntegrationTReactifN;
+  Test_PlacementGroupeN;
 end;
 /
-select * from resultat_test;
+select * from TraceTest;
 commit;
