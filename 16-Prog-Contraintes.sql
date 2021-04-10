397e4423f7cc8ff0c9bc3109e77bee93c64649bc
rebours_salome <salomereb@gmail.com>
2021-04-10 14:23:09 +0200

    MAJ contraintes

--- 16-Prog-Contraintes.sql
+++ 16-Prog-Contraintes.sql
@@ -2,54 +2,62 @@
 -- Florimel-FLOTTE
 -- Salomé-REBOURS
 
-/* Trigger 9 */
-drop trigger T_VerificationDateExp;
-create trigger T_VerificationDateExp before insert on EXPERIENCE for each row
+------------------------------------------------------------------------------------------ SOMMAIRE
+
+-- 1.   T_VerificationDateExp
+-- 2.   T_VerificationDateDemande
+-- 3.   T_VerificationNiveauAcceptation
+-- 4.   T_IntégrationReactif
+
+----------------------------------------------------------------------------- T_VerificationDateExp
+-- DébutExp <= FinExp
+
+create or replace trigger T_VerificationDateExp before insert on EXPERIENCE for each row
 begin
-    if :new.DEBUTEXP > :new.FINEXP then
-        raise_application_error(-20001, 'Les dates pour l experience ne sont pas dans le bon ordre');
+  if :new.DEBUTEXP > :new.FINEXP then
+    raise_application_error(-20001, 'Les dates de début et de fin pour une expérience ne sont pas cohérentes');
 end if;
 end;
 /
 commit;
 
-/* Trigger 12 */
-drop trigger T_VerificationDateDemande;
-create trigger T_VerificationDateDemande before insert on EXPERIENCE for each row
+------------------------------------------------------------------------- T_VerificationDateDemande
+-- DateChercheur (Date de demande de l'expérience) <= DébutExp
+  
+create or replace trigger T_VerificationDateDemande before insert on EXPERIENCE for each row
 begin
-    if :new.DEBUTEXP < :new.DATECHERCHEUR then
-        raise_application_error(-20001, 'Les dates ne sont pas dans le bon sens');
-    end if;    
+  if :new.DEBUTEXP < :new.DATECHERCHEUR then
+    raise_application_error(-20001, 'Les dates de demande et de début pour une expérience ne sont pas cohérentes');
+  end if;    
 end;
 /
-
 commit;
 
+------------------------------------------------------------------- T_VerificationNiveauAcceptation
+-- Les niveaux d'acceptation a1 et a2 sont tels que a1 <= a2
 
-/* Trigger 13 */
-drop trigger T_VerificationNiveauAcceptation;
-create trigger T_VerificationNiveauAcceptation before insert on EXPERIENCE for each row
+create or replace trigger T_VerificationNiveauAcceptation before insert on EXPERIENCE for each row
 begin
-    if :new.a1 > :new.a2 then
-        raise_application_error(-20001, 'Attention, a1 doit être < ou = de a2');
+  if :new.a1 > :new.a2 then
+    raise_application_error(-20001, 'Attention, a1 doit être < ou = de a2');
 end if;
 end;
 /
-
 commit;
 
+------------------------------------------------------------------------------ T_IntégrationReactif
+-- Eviter les redondances de réactifs : 
+-- Si le nom du réactif est déjà dans la BDD, il n'est pas à nouveau inséré
+-- (ne pas tenir compte de la casse, ni des accents)
 
-/* Empêche les doublons de nom de réactif */
-drop trigger T_IntégrationReactif;
-create trigger T_IntégrationReactif after insert on REACTIF
+create or replace trigger T_IntégrationReactif after insert on REACTIF
 declare
-nbReac integer:=0;
+  nbReac integer:=0;
 begin
-select count(*) into nbReac from
-(select count (*) from REACTIF group by nomReactif having count(*) > 1) maTable;
-if nbReac = 1 then
-        raise_application_error(-20001, 'Ce nom de réactif existe déjà');
-end if;
+  select count(*) into nbReac from (select count (*) from REACTIF group by nomReactif having count(*) > 1) maTable;
+  if nbReac = 1 then
+    raise_application_error(-20001, 'Ce réactif existe déjà');
+  end if;
 end;
 /
 commit;
